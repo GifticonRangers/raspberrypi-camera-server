2b279dd4a00049fe06006e242c0e875bf8f4ab90
jang010505 <jang010505@inu.ac.kr>
2022-12-20 05:42:52 +0900

    feat: Init run.py

---
+++ version2/run.py
@@ -0,0 +1,15 @@
+from version2.backend.server import app
+
+'''
+개발 언어: Python 3.11
+개발 환경: PyCharm
+'''
+
+version = '0.2.0'
+
+if __name__ == '__main__':
+    print('------------------------------------------------')
+    print('capstone-designs CV - version ' + version)
+    print('------------------------------------------------')
+
+    app.run(host='0.0.0.0')
